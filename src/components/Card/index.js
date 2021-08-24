e7da5c77f7299d6fc8b3fd18f9e9a1fe710c67ad
David <davidalmeida.designer@gmail.com>
2021-08-24 14:51:43 -0300

    Trabalhando componentes em ReactJS

---
+++ src/components/Card/index.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import Button from '../Button';
+
+const Card = () => {
+
+    const [valor, setValor] = useState(0)
+
+    function Adicionar () {
+        setValor(valor + 1)
+    
+    }
+    function Remover () {
+        setValor(valor - 1)
+    }
+
+        return(
+        <div className="Card">
+            <div className="card">
+                <div className="card-header">
+                    Meu primeiro Card
+                </div>
+                <div className="card-body">
+                    <Button
+                        className="btn btn-success"
+                        onClick={Adicionar}
+                    >
+                        Adicionar
+                    </Button>
+                    <Button 
+                        type="button" 
+                        className="btn btn-danger"
+                        onClick={Remover}                    
+                    >
+                        Remover
+                    </Button>
+                    <p>{valor}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
